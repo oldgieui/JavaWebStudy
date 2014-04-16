3f2d740b7073c055f3c21f945eca505db2de01bd
oldgieui <oldgieui@gmail.com>
2014-04-17 02:08:47 +0900

    ConnectionManager 기능추가 접속할 DB정보 변경이 가능해짐

--- JavaWebStudy/src/org/nhnnext/ConnectionManager.java
+++ JavaWebStudy/src/org/nhnnext/ConnectionManager.java
@@ -6,6 +6,10 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	
+	static String databaseUrl = "jdbc:mysql://localhost:3306/webDB";
+	static String databaseID = "root";
+	static String databasePW = "";
+	
 	public static Connection createConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -13,13 +17,34 @@ public class ConnectionManager {
 			System.out.println("Driver Error : " + e);
 		}
 		try {
-			String databaseUrl = "jdbc:mysql://localhost:3306/webDB";
-			String databaseID = "root";
-			String databasePW = "";
 			return DriverManager.getConnection(databaseUrl, databaseID, databasePW);
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			return null;
+		}
+	}
+	
+	public static void changeDB(String db){
+		databaseUrl = db;
+	}
+	
+	public static void changeID(String id){
+		databaseID = id;
+	}
+	
+	public static void changePW(String pw){
+		databasePW = pw;
+	}
+	
+	public static void changeDB(String db, String id, String pw) {
+		if (db != null && db != "") {
+			databaseUrl = db;
+		}
+		if (id != null && id != "") {
+			databaseID = id;
+		}
+		if (pw != null && pw!= "") {
+			databasePW = pw;
 		}
 	}
 }
