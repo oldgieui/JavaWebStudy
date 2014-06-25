f9ce43cdf370f5c97408c253531348cf63def9ac
oldgieui <oldgieui@gmail.com>
2014-06-26 04:29:04 +0900

    중복 시간대에 예약 안 되도록 처리

--- JavaWebStudy/src/org/nhnnext/dao/ResvDAO.java
+++ JavaWebStudy/src/org/nhnnext/dao/ResvDAO.java
@@ -1,5 +1,7 @@
 package org.nhnnext.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,11 +23,45 @@ public class ResvDAO extends DAO<Reservation> {
 	}
 
 	public void addResv(Reservation resv) {
-		System.out.println("Add Reservation...");
-		executeUpdate(
-				"INSERT INTO reservation(USERID, PLACENAME, PURPOSE, DATE, STARTTIME, ENDTIME) VALUES(?, ?, ?, ?, ?, ?)",
-				resv.getUserId(), resv.getPlaceName(), resv.getPurpose(),
-				resv.getDate(), resv.getStartTime(), resv.getEndTime());
+		System.out.println("Add Reservation With Transaction...");
+		String timeCheckSql = "select * from reservation where date =? and placename = ? and starttime <= ? and endtime >= ?";
+		String addSql = "INSERT INTO reservation(USERID, PLACENAME, PURPOSE, DATE, STARTTIME, ENDTIME) VALUES(?, ?, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			conn.setAutoCommit(false);
+			pstmt = conn.prepareStatement(timeCheckSql);
+			pstmt.setObject(1, resv.getDate());
+			pstmt.setObject(2, resv.getPlaceName());
+			pstmt.setObject(3, resv.getEndTime());
+			pstmt.setObject(4, resv.getStartTime());
+			// EndTime >= StartTime 이고 StartTime <= EndTime일 때 충돌. (1차원 충돌체크)
+			System.out.println(pstmt.toString());
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				conn.rollback();
+				return;
+			}
+
+			pstmt.close();
+
+			pstmt = conn.prepareStatement(addSql);
+			pstmt.setObject(1, resv.getUserId());
+			pstmt.setObject(2, resv.getPlaceName());
+			pstmt.setObject(3, resv.getPurpose());
+			pstmt.setObject(4, resv.getDate());
+			pstmt.setObject(5, resv.getStartTime());
+			pstmt.setObject(6, resv.getEndTime());
+			System.out.println(pstmt.toString());
+			pstmt.executeUpdate();
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			releaseResource(rs, pstmt, conn);
+		}
 	}
 
 	public void deleteResv(Reservation resv) {
